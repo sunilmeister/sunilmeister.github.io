1eb98991b5f94a5b02594538722914f7fb2b3f59
Sunil Nanda <65644908+sunilmeister@users.noreply.github.com>
2021-07-31 09:59:43 +0530

    Prepare for db access

--- logger/js/respimatic.logger.js
+++ logger/js/respimatic.logger.js
@@ -1,7 +1,29 @@
-var datasource_name = "RESPIMATIC100";
-var cookie_name = "respimatic_uid";
-var respimatic_uid =  sessionStorage.getItem(cookie_name);
+const datasource_name = "RESPIMATIC100";
+const cookie_name = "respimatic_uid";
+const respimatic_uid =  sessionStorage.getItem(cookie_name);
 document.title = respimatic_uid + " (LOGGER)"
+const mongo_db         = "RespimaticSystems";
+const mongo_collection = respimatic_uid ;
+const mongo_uri = 'mongodb://localhost:27017/' + mongo_db;
+
+/*
+const { MongoClient } = require('mongodb');
+const client = new MongoClient(mongo_uri);
+const db = client.db(mongo_db);
+const collection = db.collection(mongo_collection);
+
+client.connect(function(err) {
+  if (err) alert("Could not connect to MONGO server");
+});
+
+function createRecord(jsonDoc) {
+  collection.insert(jsonDoc, function (err, result) {
+    if (err) alert('Error');
+    else alert("Record added as " + result[0]._id);
+  });
+}
+
+*/
 
 function stringify_dweet(d) {
   var dweetBox = document.getElementById('dweetBox');
@@ -20,17 +42,12 @@ function process_dweet_content(d) {
     value = d.content[key];
   }
 
-  return d;
 }
 
 function wait_for_dweets() {
-  window.setInterval(function() {
-    var elem = document.getElementById('dweetBox');
-    elem.scrollTop = elem.scrollHeight;
-  }, 3000);
-
   dweetio.listen_for(respimatic_uid, function(d) {
-    //process_dweet_content(d);
+    process_dweet_content(d);
     stringify_dweet(d);
   });
 }
+
