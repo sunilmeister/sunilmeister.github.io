3f1195dc33fe59cd8d7bb26f8292deb9c4fa9195
Sunil Nanda <65644908+sunilmeister@users.noreply.github.com>
2021-08-03 16:10:25 +0530

    IndexedDB database first cut

--- logger/js/respimatic.logger.js
+++ logger/js/respimatic.logger.js
@@ -1,36 +1,196 @@
-const datasource_name = "RESPIMATIC100";
-const cookie_name = "respimatic_uid";
-const respimatic_uid =  sessionStorage.getItem(cookie_name);
-document.title = respimatic_uid + " (LOGGER)"
-const mongo_db         = "RespimaticSystems";
-const mongo_collection = respimatic_uid ;
-const mongo_uri = 'mongodb://localhost:27017/' + mongo_db;
+const cookieName = "respimatic_uid";
+const respimaticUid =  sessionStorage.getItem(cookieName);
+document.title = respimaticUid + " (LOGGER)" ;
+const dbNamePrefix = respimaticUid ;
+const dbName= dbNamePrefix + '#' + new Date() ;
+const dbVersion = 1;
+const dbObjStoreName = respimaticUid ;
+const tableName = respimaticUid ;
+const localStorageDbName = "respimatic_dbs" ;
+var doLog = false;
 
-/*
-const { MongoClient } = require('mongodb');
-const client = new MongoClient(mongo_uri);
-const db = client.db(mongo_db);
-const collection = db.collection(mongo_collection);
-
-client.connect(function(err) {
-  if (err) alert("Could not connect to MONGO server");
-});
-
-function createRecord(jsonDoc) {
-  collection.insert(jsonDoc, function (err, result) {
-    if (err) alert('Error');
-    else alert("Record added as " + result[0]._id);
-  });
+// check for browser capability
+if (!window.indexedDB) {
+    console.log("Your browser doesn't support a stable version of IndexedDB.");
+    alert("IndexedDB not available in your browser. Switch browsers");
 }
 
-*/
+// ///////////////////////////////////////////////////////
+// Open/Create Database 
+// ///////////////////////////////////////////////////////
+var db;
+var dbReq = indexedDB.open(dbName, dbVersion);
+var dbReady = false;
 
-function stringify_dweet(d) {
+// Fires when the version of the database goes up, or the database is created
+// for the first time
+dbReq.onupgradeneeded = function(event) {
+  db = event.target.result;
+
+  // Object stores in databases are where data are stored.
+  let dbObjStore;
+  if (!db.objectStoreNames.contains(dbObjStoreName)) {
+    dbObjStore = db.createObjectStore(dbObjStoreName, {autoIncrement: true});
+  } else {
+    dbObjStore = dbReq.transaction.objectStore(dbObjStoreName);
+  }
+}
+
+// Fires once the database is opened (and onupgradeneeded completes, if
+// onupgradeneeded was called)
+dbReq.onsuccess = function(event) {
+  // Set the db variable to our database so we can use it!  
+  db = event.target.result;
+  dbReady = true;
+
+  // Keep track of databases currently existing
+  var retrieved_dbs = localStorage.getItem(localStorageDbName);
+  var respimatic_dbs = [];
+  if (retrieved_dbs) {
+    respimatic_dbs = JSON.parse(retrieved_dbs);
+  }
+
+  var ix;
+  if (respimatic_dbs.length) {
+    ix = respimatic_dbs.indexOf(dbName);
+  } else {
+    ix = -1;
+  }
+
+  if (ix==-1) {
+    respimatic_dbs.push(dbName);
+    localStorage.setItem(localStorageDbName, JSON.stringify(respimatic_dbs));
+  }
+ 
+  //alert("Created " + dbName);
+}
+
+// Fires when we can't open the database
+dbReq.onerror = function(event) {
+  dbReady = false;
+  alert('Error opening database ' + event.target.errorCode);
+}
+
+// Fires when there's another open connection to the same database
+dbReq.onblocked = function(event) {
+  dbReady = false;
+  db = event.target.result;
+  db.close();
+  alert("Database is updated, Close all LOGGER tabs, reload the page.");
+}
+
+// ///////////////////////////////////////////////////////
+// Database Functions 
+// ///////////////////////////////////////////////////////
+function listDb(item, index) {
+  nameTm = parseDbName(item);
+  alert(index + " " + nameTm[0] + "\nCreated:" + nameTm[1]);
+}
+
+function getAllDbs() {
+  var str = localStorage.getItem(localStorageDbName);
+  var retrieved_dbs = JSON.parse(str);
+  return retrieved_dbs;
+}
+
+function listDbTableRow(item, index) {
+  // do not list current db
+  if (item==dbName) return;
+
+  nameTm = parseDbName(item);
+
+  // only list databases for the currently selected system
+  if (nameTm[0] != respimaticUid) return;
+
+  var table = document.getElementById("dbTable");
+
+  var row = table.insertRow();
+
+  var cell = row.insertCell();
+  cell.innerHTML = nameTm[1];
+  cell = row.insertCell();
+  cell.innerHTML = '<button class="tableButton" onclick="deleteDbRow(this)">DELETE</button>' ;
+
+}
+
+function listAllDbs() {
+  //clear any existing table being shown
+  var table = document.getElementById("dbTable");
+  table.innerHTML = "";
+
+  var retrieved_dbs = getAllDbs();
+  retrieved_dbs.forEach(listDbTableRow);
+}
+
+function deleteDb(dbName) {
+  // Keep track of databases currently existing
+  var retrieved_dbs = localStorage.getItem(localStorageDbName);
+  var respimatic_dbs = [];
+  if (retrieved_dbs) {
+    respimatic_dbs = JSON.parse(retrieved_dbs);
+  }
+
+  var ix;
+  if (respimatic_dbs.length) {
+    ix = respimatic_dbs.indexOf(dbName);
+  } else {
+    ix = -1;
+  }
+
+  if (ix!=-1) {
+    respimatic_dbs.splice(ix, 1);
+    localStorage.setItem(localStorageDbName, JSON.stringify(respimatic_dbs));
+  }
+ 
+  var request = indexedDB.deleteDatabase(dbName);
+  return request;
+}
+
+function deleteDbRow(row) {
+  var p=row.parentNode.parentNode;
+
+  // reconstruct the dbName
+  // grab the creation field from the first cell in the same row
+  var dName = respimaticUid + '#' + p.cells[0].innerHTML;
+
+  if (!confirm("Delete Database created on\n" + p.cells[0].innerHTML)) {
+    return;
+  }
+
+  // Delete the actual database
+  deleteDb(dName);
+
+  // remove from HTML table
+  p.parentNode.removeChild(p);
+}
+
+function insertJsonData(db,jsonData) {
+  // Start a database transaction and get the notes object store
+  let tx = db.transaction([dbObjStoreName], 'readwrite');
+  let store = tx.objectStore(dbObjStoreName);  
+  store.add(jsonData);  // Wait for the database transaction to complete
+  tx.oncomplete = function() { console.log('stored data!') }
+  tx.onerror = function(event) {
+    alert('error storing data! ' + event.target.errorCode);
+  }
+}
+
+function parseDbName(name) {
+  // dbNames are of the form RSP_XXXXXXXXXXXX:Date
+  arr = name.split('#');
+  return arr;
+}
+
+// ///////////////////////////////////////////////////////
+// Dweet Functions 
+// ///////////////////////////////////////////////////////
+
+function stringifyDweet(d) {
   var dweetBox = document.getElementById('dweetBox');
   dweetBox.innerText = dweetBox.textContent = JSON.stringify(d,null,". ") ;
 }
 
-function process_dweet_content(d) {
+function processDweet(d) {
   // extract all info from the dweet
   // and then shove it into a database
 
@@ -44,10 +204,27 @@ function process_dweet_content(d) {
 
 }
 
-function wait_for_dweets() {
+function waitForDweets() {
   dweetio.listen_for(respimatic_uid, function(d) {
-    process_dweet_content(d);
-    stringify_dweet(d);
+    processDweet(d);
+    stringifyDweet(d);
   });
+}
+
+// ///////////////////////////////////////////////////////
+// MAIN function executed on window load
+// ///////////////////////////////////////////////////////
+
+function manageDb() {
+  var heading = document.getElementById("SysUid");
+  heading.innerHTML = "LOG for " + respimaticUid;
+}
+
+function startLog() {
+  doLog = true;
+}
+
+function pauseLog() {
+  doLog = false;
 }
 
