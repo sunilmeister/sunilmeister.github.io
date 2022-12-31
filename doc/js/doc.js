f826329c261d2d44c77133e0741540a1f09466f2
Sunil Nanda <65644908+sunilmeister@users.noreply.github.com>
2022-12-31 13:24:59 +0530

    View docs one at a time

---
+++ doc/js/doc.js
@@ -0,0 +1,19 @@
+function opManual() {
+  document.getElementById('opManualDiv').style.display="block";
+  document.getElementById('webAppsDiv').style.display="none";
+  document.getElementById('slideDeckDiv').style.display="none";
+}
+
+function webApps() {
+  document.getElementById('opManualDiv').style.display="none";
+  document.getElementById('webAppsDiv').style.display="block";
+  document.getElementById('slideDeckDiv').style.display="none";
+}
+
+function slideDeck() {
+  document.getElementById('opManualDiv').style.display="none";
+  document.getElementById('webAppsDiv').style.display="none";
+  document.getElementById('slideDeckDiv').style.display="block";
+}
+
+
