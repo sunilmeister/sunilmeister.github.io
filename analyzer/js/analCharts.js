353b444a9cc20fd82cb021b095187ad1d3bd40aa
Sunil Nanda <65644908+sunilmeister@users.noreply.github.com>
2023-03-08 13:13:58 +0530

    Update analCharts.js

--- analyzer/js/analCharts.js
+++ analyzer/js/analCharts.js
@@ -34,6 +34,7 @@ function initCharts() {
   for (id in session.charts.allChartsContainerInfo) {
     allChartDivs.push(id);
   }
+  session.charts.allChartsContainerInfo = {};
   for (i=0; i<allChartDivs.length; i++) {
     id = allChartDivs[i];
     node = document.getElementById(id);
