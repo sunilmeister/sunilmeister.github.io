73d6b688166d04557378e98f39e05dfdf473b63e
Sunil Nanda <65644908+sunilmeister@users.noreply.github.com>
2023-03-08 16:02:24 +0530

    Update common.js

--- common/js/common.js
+++ common/js/common.js
@@ -58,7 +58,21 @@ function cloneObject(obj) {
   return JSON.parse(JSON.stringify(obj));
 }
 
-function parseJSONSafely(str) {
+function shallowEqualObj(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+  for (let key of keys1) {
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+  function parseJSONSafely(str) {
   try {
     return JSON.parse(str);
   } catch (e) {
