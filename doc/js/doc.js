e22a39459929d5afa81ec906d6416f95af7d54af
Sunil Nanda <65644908+sunilmeister@users.noreply.github.com>
2023-06-02 16:56:33 +0530

    Added FAQ to docs

--- doc/js/doc.js
+++ doc/js/doc.js
@@ -4,64 +4,91 @@
 
 function enable(btn, img) {
   var style = getComputedStyle(document.body)
-  bgd = style.getPropertyValue('--rsp_lightblue');
+  bgd = style.getPropertyValue('--rsp_mediumblue');
   document.getElementById(btn).style.backgroundColor = bgd;
+  document.getElementById(btn).style.color = 'white';
   document.getElementById(btn).style.cursor = "pointer";
   document.getElementById(img).style.cursor = "pointer";
 }
 
 function disable(btn, img) {
-  document.getElementById(btn).style.backgroundColor = "white";
+  var style = getComputedStyle(document.body)
+  bgd = style.getPropertyValue('--rsp_lightblue');
+  fgd = style.getPropertyValue('--rsp_mediumblue');
+  document.getElementById(btn).style.backgroundColor = bgd;
+  document.getElementById(btn).style.color = fgd;
   document.getElementById(btn).style.cursor = "not-allowed";
   document.getElementById(img).style.cursor = "not-allowed";
 }
 
+function faqManual() {
+  document.getElementById('faqManualDiv').style.display = "block";
+  document.getElementById('opManualDiv').style.display = "none";
+  document.getElementById('webAppsDiv').style.display = "none";
+  document.getElementById('slideDeckDiv').style.display = "none";
+  document.getElementById('firmwareDeckDiv').style.display = "none";
+
+  disable('btnFaqManual', 'faqManualImg');
+  enable('btnOpManual', 'opManualImg');
+  enable('btnWebApps', 'webAppsImg');
+  enable('btnSlideDeck', 'slideDeckImg');
+  enable('btnFirmwareDeck', 'firmwareDeckImg');
+}
+
 function opManual() {
+  document.getElementById('faqManualDiv').style.display = "none";
   document.getElementById('opManualDiv').style.display = "block";
   document.getElementById('webAppsDiv').style.display = "none";
   document.getElementById('slideDeckDiv').style.display = "none";
   document.getElementById('firmwareDeckDiv').style.display = "none";
 
-  disable('opManualBtn', 'opManualImg');
-  enable('webAppsBtn', 'webAppsImg');
-  enable('slideDeckBtn', 'slideDeckImg');
-  enable('firmwareDeckBtn', 'firmwareDeckImg');
+  enable('btnFaqManual', 'faqManualImg');
+  disable('btnOpManual', 'opManualImg');
+  enable('btnWebApps', 'webAppsImg');
+  enable('btnSlideDeck', 'slideDeckImg');
+  enable('btnFirmwareDeck', 'firmwareDeckImg');
 }
 
 function webApps() {
   document.getElementById('opManualDiv').style.display = "none";
+  document.getElementById('faqManualDiv').style.display = "none";
   document.getElementById('webAppsDiv').style.display = "block";
   document.getElementById('slideDeckDiv').style.display = "none";
   document.getElementById('firmwareDeckDiv').style.display = "none";
 
-  enable('opManualBtn', 'opManualImg');
-  disable('webAppsBtn', 'webAppsImg');
-  enable('slideDeckBtn', 'slideDeckImg');
-  enable('firmwareDeckBtn', 'firmwareDeckImg');
+  enable('btnFaqManual', 'faqManualImg');
+  enable('btnOpManual', 'opManualImg');
+  disable('btnWebApps', 'webAppsImg');
+  enable('btnSlideDeck', 'slideDeckImg');
+  enable('btnFirmwareDeck', 'firmwareDeckImg');
 }
 
 function slideDeck() {
+  document.getElementById('faqManualDiv').style.display = "none";
   document.getElementById('opManualDiv').style.display = "none";
   document.getElementById('webAppsDiv').style.display = "none";
   document.getElementById('slideDeckDiv').style.display = "block";
   document.getElementById('firmwareDeckDiv').style.display = "none";
 
-  enable('opManualBtn', 'opManualImg');
-  enable('webAppsBtn', 'webAppsImg');
-  disable('slideDeckBtn', 'slideDeckImg');
-  enable('firmwareDeckBtn', 'firmwareDeckImg');
+  enable('btnFaqManual', 'faqManualImg');
+  enable('btnOpManual', 'opManualImg');
+  enable('btnWebApps', 'webAppsImg');
+  disable('btnSlideDeck', 'slideDeckImg');
+  enable('btnFirmwareDeck', 'firmwareDeckImg');
 }
 
 function firmwareDeck() {
+  document.getElementById('faqManualDiv').style.display = "none";
   document.getElementById('opManualDiv').style.display = "none";
   document.getElementById('webAppsDiv').style.display = "none";
   document.getElementById('slideDeckDiv').style.display = "none";
   document.getElementById('firmwareDeckDiv').style.display = "block";
 
-  enable('opManualBtn', 'opManualImg');
-  enable('webAppsBtn', 'webAppsImg');
-  enable('slideDeckBtn', 'slideDeckImg');
-  disable('firmwareDeckBtn', 'firmwareDeckImg');
+  enable('btnFaqManual', 'faqManualImg');
+  enable('btnOpManual', 'opManualImg');
+  enable('btnWebApps', 'webAppsImg');
+  enable('btnSlideDeck', 'slideDeckImg');
+  disable('btnFirmwareDeck', 'firmwareDeckImg');
 }
 
 window.onload = function () {
@@ -107,12 +134,12 @@ window.onload = function () {
       }
     })
   }
-  var menuBar = document.getElementById("sideMenuBar");
-  menuBarHeight = menuBar.offsetHeight;
-  menuBarWidth = menuBar.offsetWidth;
-  var nonMenuArea = document.getElementById("nonMenuArea");
-  nonMenuArea.style.marginTop = String(0 - menuBarHeight) + "px";
-  nonMenuArea.style.marginLeft = String(menuBarWidth + 50) + "px";
+  //var menuBar = document.getElementById("sideMenuBar");
+  //menuBarHeight = menuBar.offsetHeight;
+  //menuBarWidth = menuBar.offsetWidth;
+  //var nonMenuArea = document.getElementById("nonMenuArea");
+  //nonMenuArea.style.marginTop = String(0 - menuBarHeight) + "px";
+  //nonMenuArea.style.marginLeft = String(menuBarWidth + 50) + "px";
   //console.log("menuBarHeight = " + menuBarHeight);
   //console.log("menuBarWidth = " + menuBarWidth);
 }
