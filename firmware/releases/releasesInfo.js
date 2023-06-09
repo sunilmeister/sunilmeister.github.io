4186cb64a2a951b59994036437b661b65beac992
Sunil Nanda <65644908+sunilmeister@users.noreply.github.com>
2023-06-09 19:06:44 +0530

    Update 2.0.1 release

--- firmware/releases/releasesInfo.js
+++ firmware/releases/releasesInfo.js
@@ -6,7 +6,7 @@
 // ////////////////////////////////////////////////////
 
 var knownRespimaticReleases = [
-  {rel:"2.0.1", created:"09-Jun-2023"},
+  {rel:"2.0.1", created:"10-Jun-2023"},
   {rel:"1.1.1", created:"30-May-2023"},
   {rel:"1.0.1", created:"16-May-2023"},
 ];
