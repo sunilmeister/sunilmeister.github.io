78840203eb3680e4d633136e64721828f6cee764
Sunil Nanda <65644908+sunilmeister@users.noreply.github.com>
2023-07-15 08:14:02 +0530

    Update recorder.js

--- recorder/js/recorder.js
+++ recorder/js/recorder.js
@@ -9,6 +9,7 @@ var simulatedMillis = 0;
 var lastDweetInMs = 0;
 var startSystemDate = new Date();
 var awaitingFirstDweet = true;
+var dweetQ;
 const INIT_RECORDING_INTERVAL_IN_MS = 5000;
 const MAX_DIFF_DWEET_SIMULAION_TIMES = 10000;
 
