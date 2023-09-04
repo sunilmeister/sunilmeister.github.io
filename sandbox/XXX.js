586fd8e678b9ca3dc8684eae52894f3bf18faa07
Sunil Nanda <65644908+sunilmeister@users.noreply.github.com>
2023-09-04 18:04:21 +0530

    testApi

--- sandbox/XXX.js
+++
@@ -1,7 +0,0 @@
- // This is the api implementation
- // callbackFn has 2 arguments (timeStamp, jsonData)
- function XXX(uid, callbackFn) {
-   console.log("Received uid = " + uid);
-   jsonData = {a:1, b:2, c:3};
-   callbackFn(Date(), jsonData);
- }
