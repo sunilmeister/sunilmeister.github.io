c3765a1aef948359dc747a292504a48766cf1dda
Sunil Nanda <65644908+sunilmeister@users.noreply.github.com>
2023-10-05 19:32:23 +0530

    preparing for homegrown dweet

--- common/js/respimaticMessages.js
+++ common/js/respimaticMessages.js
@@ -27,53 +27,54 @@ function waitForRespimaticMessages(uidString, callbackFn) {
 // These are private functions
 ////////////////////////////////////////////////////////////////////////////////
 var prevResponseTimestamp = Date();
+var respimaticListenIntervalId = null;
+
+function executeRespimaticListenFor(uidString, callbackFn) {
+  $.ajax({
+    url: 'http://respimaticlisten.atmanirbhar.org/display_json_response',
+    method: 'POST',
+    data: {uid: uidString},
+    success: function (response) {
+      var timestamp = response.timestamp;
+      if (!timestamp || $.isEmptyObject(timestamp) ) return;
+
+      var jsonObject = response.jsonData;
+      if (jsonObject === null) return;
+      if (typeof jsonObject !== 'object') return;
+      if (Object.keys(jsonObject).length == 0) return;
+
+      if (prevResponseTimestamp == timestamp) {
+        // This is a repeat - do not call the callbackFn
+        return;
+      }
+
+      if (response.data == 'success') {
+        prevResponseTimestamp = timestamp;
+        console.log(timestamp,jsonObject);
+        // change the response to be in dweet format
+        // so that the rest of the code does not have to change
+        // when switching from dweet to respimaticListenFor
+        var dweetObj = dweetFormat(uidString, timestamp, jsonObject.response.content);
+        callbackFn(dweetObj); // Pass the response to the callbackFn
+      }
+    }
+  });
+}
+
 function respimaticListenFor(uidString, callbackFn) {
-  // Call the real work function immediately
   executeRespimaticListenFor(uidString, callbackFn);
 
-  // After that call the real work function every 10ms
-  intervalId = setInterval(executeRespimaticListenFor(uidString, callbackFn), 
-                           LISTEN_INTERVAL_IN_MS);
+  // After that call the real work function every LISTEN_INTERVAL_IN_MS
+  respimaticListenIntervalId = setInterval(
+        () => executeRespimaticListenFor(uidString, callbackFn),
+        LISTEN_INTERVAL_IN_MS);
 }
 
-// This function does the actual listening
-function executeRespimaticListenFor(uidString, callbackFn) {
- $.ajax({
-      url: 'http://respimaticlisten.atmanirbhar.org/display_json_response',
-      method: 'POST',
-      data: {uid: uidString},
-      success: function (response) {
-        var timestamp = response.timestamp;
-        if (prevResponseTimestamp == timestamp) {
-          // This is a repeat - do not call the callbackFn
-          return;
-        } else {
-          prevResponseTimestamp = timestamp;
-        }
-
-        console.log(response)
-        var jsonObject = response.jsonData;
-        if (jsonObject === null) return;
-        if (typeof jsonObject !== 'object') return;
-        if (Object.keys(jsonObject).length === 0) return;
-
-        if (response.data == 'success') {
-          console.log(timestamp,jsonObject);
-          // change the response to be in dweet format
-          // so that the rest of the code does not have to change
-          // when switching from dweet to respimaticListenFor
-          var dweetObj = imitateDweetFormat(uidString, timeStamp, jsonObject);
-          callbackFn(dweetObj); // Pass the response to the callbackFn
-        }
-      }
-    });
-}
-
-function imitateDweetFormat(uidString, timeStamp, jsonObject) {
+function dweetFormat(uidString, timeStamp, jsonContent) {
   var dweetObj = {
     "thing": uidString,
     "created": new Date(timeStamp),
-    "content" : jsonObject.content,
+    "content" : jsonContent,
   };
-  return dweetObj;
+  return cloneObject(dweetObj);
 }
