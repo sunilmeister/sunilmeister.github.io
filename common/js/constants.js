b6f33d549892bedf3e7ab44f274b78a9378566aa
Sunil Nanda <sunil.nanda@yahoo.com>
2024-02-29 09:14:44 +0530

    Switching to new api

--- common/js/constants.js
+++ common/js/constants.js
@@ -7,8 +7,8 @@ const CURRENT_RECORDING_VERSION = "1.0";
 // Switch between using dweetio.listen_for and inspireListenFor
 // by changing the constant USE_DWEET_FOR_MESSAGES
 ////////////////////////////////////////////////////////////////////////////////
-const USE_DWEET_FOR_MESSAGES = true;
-const MESSAGE_LISTEN_INTERVAL_IN_MS = 330;
+const USE_DWEET_FOR_MESSAGES = false;
+const MESSAGE_LISTEN_INTERVAL_IN_MS = 500;
 
 const INSPIRE_UID_PREFIX = "UID_";
 const DASHBOARD_APP_ID = "DASHBOARD";
