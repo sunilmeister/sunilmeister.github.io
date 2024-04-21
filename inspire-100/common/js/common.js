16e7fa16340d1e5b52a9c638576e2c44184f2fae
Sunil Nanda <sunil.nanda@yahoo.com>
2024-04-21 19:28:50 +0530

    Update common.js

--- inspire-100/common/js/common.js
+++ inspire-100/common/js/common.js
@@ -30,7 +30,7 @@ function setRootFontSize(orientation) {
 	let root = document.documentElement;
 	let width = window.innerWidth / window.devicePixelRatio;
 	let height = window.innerHeight / window.devicePixelRatio;
-	console.log("height", height, "width", width, "pxRatio", window.devicePixelRatio, "appScaleFactor", appScaleFactor);
+	//console.log("height", height, "width", width, "pxRatio", window.devicePixelRatio, "appScaleFactor", appScaleFactor);
 
 	let fontSize = 18 * appScaleFactor;
 	let totalSize = 0;
@@ -44,7 +44,7 @@ function setRootFontSize(orientation) {
 
 	if (fontSize > 18) fontSize = 18;
 	if (fontSize < 4) fontSize = 4;
-	console.log(orientation,"totalSize", totalSize, "root font-size", fontSize);
+	console.log(totalSize", totalSize, "root font-size", fontSize);
  	root.style.fontSize = String(fontSize) + "px";
 	if (appResizeFunction) appResizeFunction();
 }
