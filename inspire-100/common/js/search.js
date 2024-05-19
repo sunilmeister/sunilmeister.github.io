60807b3786a54ff0dffd98f3111624f3b74ec940
Sunil Nanda <sunil.nanda@yahoo.com>
2024-05-19 13:04:38 +0530

    Get rid of sandbox searchExpr

--- inspire-100/common/js/search.js
+++ inspire-100/common/js/search.js
@@ -2,21 +2,4 @@
 // Author: Sunil Nanda
 // ////////////////////////////////////////////////////
 
-// Same DIV is shared for various exports by changing the onclick function
-function exportSearchExpr() {
-  let exportBtn = document.getElementById("exportFileBtn");
-	exportBtn.onclick = function() { exportExprFile() };
-	let exportDiv = document.getElementById("exportDiv");
-	exportDiv.style.display = "block";
-}
-
-function importSearchExpr() {
-	let importDiv = document.getElementById("importExprDiv");
-	importDiv.style.display = "block";
-}
-
-function cancelExprImport() {
-	let importDiv = document.getElementById("importExprDiv");
-	importDiv.style.display = "none";
-}
 
--- inspire-100/common/js/searchExpr.js
+++ inspire-100/common/js/searchExpr.js
@@ -894,3 +894,22 @@ function exportExprFile() {
 function clearSearchExpr() {
 	session.searchExpression.clearExpr();
 }
+
+// Same DIV is shared for various exports by changing the onclick function
+function exportSearchExpr() {
+  let exportBtn = document.getElementById("exportFileBtn");
+	exportBtn.onclick = function() { exportExprFile() };
+	let exportDiv = document.getElementById("exportDiv");
+	exportDiv.style.display = "block";
+}
+
+function importSearchExpr() {
+	let importDiv = document.getElementById("importExprDiv");
+	importDiv.style.display = "block";
+}
+
+function cancelExprImport() {
+	let importDiv = document.getElementById("importExprDiv");
+	importDiv.style.display = "none";
+}
+
