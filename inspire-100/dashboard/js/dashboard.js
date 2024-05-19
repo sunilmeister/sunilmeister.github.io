fee3bec2eaa81361a8d148cf22da79b361e6deb7
Sunil Nanda <sunil.nanda@yahoo.com>
2024-05-19 12:27:26 +0530

    Sidebar formatting

--- inspire-100/dashboard/js/dashboard.js
+++ inspire-100/dashboard/js/dashboard.js
@@ -600,7 +600,7 @@ function alignSidebar() {
   let menuBarHeight = menuBar.offsetHeight;
   let menuBarWidth = menuBar.offsetWidth;
   let nonMenuArea = document.getElementById("nonMenuArea");
-  nonMenuArea.style.marginTop = String(convertPixelsToRem(0 - menuBarHeight)) + "rem";
+  nonMenuArea.style.marginTop = String(convertPixelsToRem(0 - menuBarHeight) + 1) + "rem";
   nonMenuArea.style.marginLeft = String(convertPixelsToRem(menuBarWidth +30)) + "rem";
 }
 
